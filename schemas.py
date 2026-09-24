d91339a0ac6ad0aa85022f6c68dc36689c630fa2
Schadrack Karekezi <vitalshadrackk@gmail.com>
2026-09-24 16:51:21 -0700

    Add auth, database models, and user/post routers

--- schemas.py
+++ schemas.py
@@ -1,21 +1,49 @@
 # creating pydantic schemas and validation model
+# what is pydantic is data validation libray that uses python type hints and it enforces them at runtime and give you error messages if they are not passed id 
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+from datetime import datetime
 
-from pydantic import BaseModel, ConfigDict, Field
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_length=120)
+class UserCreate(UserBase):
+    password: str = Field(min_length=8)
+class UserPublic(BaseModel):
+    model_config = ConfigDict(from_attributes=True) 
+    
+    id: int
+    username: str
+    image_file: str | None
+    image_path: str
+class UserPrivate(UserPublic):
+    email: EmailStr
+    
+class UserUpdate(BaseModel):
+    username: str | None = Field(default=None, min_length=1, max_length=50)
+    email: EmailStr | None = Field(default=None, max_length=120)
+    image_file: str | None = Field(default=None, min_length=1, max_length=200)
 
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    
 class PostBase(BaseModel):
     title: str = Field(min_length=1 , max_length=100)
     content: str = Field(min_length= 1)
-    author: str = Field(min_length= 1, max_length=50)
     
-
 class PostCreate(PostBase):
-    pass
-
+    user_id : int #Temporary
+       
+class PostUpdate(BaseModel):
+    title: str | None = Field(default=None, min_length=1 , max_length=100)
+    content: str | None = Field(default=None, min_length= 1)
+    
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True) ## from_attributes allow to read values in the dict
     id: int
-    date_posted: str
+    user_id: int
+    date_posted: datetime
+    author : UserPublic
 
-      
-    
 
